f1a48add6be1532c1a8371bc30190a2ba9d378a7
Chet <chethana96@gmail.com>
2019-06-11 21:14:36 +1200

    finding alternative for nodemailer-mailgun-transport module

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,6 +1,9 @@
+const sendEmail = require("../lambdas/mailer");
+
 exports.handler = function (event, context, callback) {
 
   // send mail here
+  sendEmail("chethana96@gmail.com");
 
   callback(null, {
     statusCode: 200,
--- lambdas/mailer.js
+++ lambdas/mailer.js
@@ -1,25 +1,31 @@
 const mg = require("nodemailer-mailgun-transport");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const sendVerification = async function ({ name, email, message }) {
-    const auth = {
-        auth: {
-            api_key: MAILGUN_KEY,
-            domain: MAILGUN_API
-        }
-    };
-    const nodemailerMailgun = nodemailer.createTransport(mg(auth));
-    nodemailerMailgun.sendMail({
-        from: 'contact@chet-w.com',
-        to: email,
-        subject: `New enquiry at chet-w.com`,
-        html: ``
-    }, (err, info) => {
-        if (err) {
-            console.log(`Error: ${err}`);
-        }
-        else {
-            console.log(`Response: ${info}`);
-        }
-    });
-}
+// const MAILGUN_API = require("../config").MAILGUN_API;
+// const MAILGUN_KEY = require("../config").MAILGUN_KEY;
+
+// const sendMail = async function (email) {
+//     const auth = {
+//         auth: {
+//             api_key: MAILGUN_KEY,
+//             domain: MAILGUN_API
+//         }
+//     };
+//     const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+//     nodemailerMailgun.sendMail({
+//         from: 'contact@chet-w.com',
+//         to: email,
+//         subject: `New enquiry at chet-w.com`,
+//         html: ``
+//     }, (err, info) => {
+//         if (err) {
+//             console.log(`Error: ${err}`);
+//         }
+//         else {
+//             console.log(`Response: ${info}`);
+//         }
+//     });
+// };
+console.log("hello")
+
+module.exports = sendMail;
