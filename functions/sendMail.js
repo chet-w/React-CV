7e2dff02ff97778f00705f3f0be619df1b93b62c
Chet <chethana96@gmail.com>
2019-06-16 23:18:49 +1200

    tentitive deploy with mail delivery

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -3,10 +3,5 @@ const sendEmail = require("../lambdas/mailer");
 exports.handler = function (event, context, callback) {
 
   // send mail here
-  sendEmail();
-
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+  sendEmail("chethana96@gmail.com", callback);
 }
--- lambdas/mailer.js
+++ lambdas/mailer.js
@@ -1,48 +1,50 @@
 
-const sendEmail = function() {
+const sendEmail = function (target, callback) {
 
-const nodemailer = require("nodemailer");
-const smtpTransport = require('nodemailer-smtp-transport');
+    const nodemailer = require("nodemailer");
+    const smtpTransport = require('nodemailer-smtp-transport');
 
-const MAILGUN_PASS = require("../config").MAILGUN_PASS;
-const MAILGUN_USER = require("../config").MAILGUN_USER;
+    const MAILGUN_PASS = require("../config").MAILGUN_PASS || NET_MAILGUN_PASS;
+    const MAILGUN_USER = require("../config").MAILGUN_USER || NET_MAILGUN_USER;
 
-const transporter = nodemailer.createTransport(smtpTransport({
-    service: "Mailgun",
-    auth: {
-        user: MAILGUN_USER,
-        pass: MAILGUN_PASS
-    }
-}));
+    const transporter = nodemailer.createTransport(smtpTransport({
+        service: "Mailgun",
+        auth: {
+            user: MAILGUN_USER,
+            pass: MAILGUN_PASS
+        }
+    }));
 
-var text = 'Email body goes here';
-//
-var mailOptions = {
-    from: 'enquiries@chet-w.com',
-    to: 'chethana96@gmail.com',
-    subject: 'New enquiry from Chet-w.com',
-    text: text
-};
+    var text = 'Email body goes here';
+    //
+    var mailOptions = {
+        from: 'enquiries@chet-w.com',
+        to: "chethana96@gmail.com",
+        subject: 'New enquiry from Chet-w.com',
+        text: text
+    };
 
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        // const response = {
-        //   statusCode: 500,
-        //   body: JSON.stringify({
-        //     error: error.message,
-        //   }),
-        // };
-        console.log(error);
-    } else {
-        console.log("Done!");
-    }
-    //   const response = {
-    //     statusCode: 200,
-    //     body: JSON.stringify({
-    //       message: `Email processed succesfully!`
-    //     }),
-    //   };
-});
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            const response = {
+              statusCode: 500,
+              body: JSON.stringify({
+                error: error.message,
+              }),
+            };
+            console.log(error);
+            callback(null, response);
+        } else {
+            console.log("Done!");
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                  message: `Email processed succesfully!`
+                }),
+              };
+              callback(null, response);
+        }
+    });
 
 };
 
